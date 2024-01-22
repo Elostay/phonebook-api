62f1e52b44bb5d1a9408f79ac78a7d69b376c02a
Elostay <nastia60203@gmail.com>
2024-01-22 19:39:14 +0200

    server try

---
+++ server.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
